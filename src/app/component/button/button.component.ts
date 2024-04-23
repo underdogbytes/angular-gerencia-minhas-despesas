7c3ec98168d569cf52eeb016967d78a8e32ca1fb
bea <beatriztattooer@gmail.com>
2024-04-22 21:55:32 -0300

    :construction: WIP: header & buttons
    Aprendi a colocar rotas e two way databinding :D

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+  @Input() label = '';
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  home = 'Home';
+  about = 'Sobre nós'
 }

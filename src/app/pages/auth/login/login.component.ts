58edd33f65d6832173f1e93e39b9ff3c58bdb096
bea <beatriztattooer@gmail.com>
2024-09-23 19:20:32 -0300

    :sparkles: feat[login]: auth with validator
    - Just email & password

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,33 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgFor,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  authForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.authForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: ['', Validators.required]
+    })
+  }
+
+  login() {
+    if (this.authForm.valid) {
+      console.log("Falso login efetuado! \nDados:", this.authForm.value);
+    }
+  }
 }

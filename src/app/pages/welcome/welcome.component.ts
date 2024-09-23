e534f5274c86dae0c1893f17f21eb733dbb063c4
bea <beatriztattooer@gmail.com>
2024-09-23 18:54:39 -0300

    :sparkles: feat[page]: welcome login btn
    Implemented login btn at root page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../component/button/button.component";
+import { NavigationService } from './../../services/navigation/navigation.service';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
-
+  constructor(private navigationService: NavigationService) { }
+  
+  irPara(rota: string) {
+    this.navigationService.mudarRota(rota)
+  }
 }
